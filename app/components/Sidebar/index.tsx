94767e97eb41dd52ebcb6aab2c00fd2dad3f7724
Arnaud Pichon <hi@onesime.fr>
2019-03-11 13:51:48 +0100

    integrate sidebar

---
+++ app/components/Sidebar/index.tsx
@@ -0,0 +1,44 @@
+/**
+ *
+ * Sidebar
+ *
+ */
+
+import * as React from 'react';
+import { FeatureCollection } from 'geojson';
+import './styles.css';
+
+function MenuItem(props) {
+  console.log(props.data);
+  return (
+    <h1>Menu Item</h1>
+  );
+}
+
+interface OwnProps {
+  annotations: FeatureCollection;
+}
+
+const Sidebar: React.SFC<OwnProps> = (props: OwnProps) => {
+  if (!props.annotations) {
+    return (
+      <div className="sidebar">
+        <h1>Select a slide</h1>
+      </div>
+    );
+  }
+  if (props.annotations.features.length === 0) {
+    return (
+      <div className="sidebar">
+        <h1>Edit your annotations here.</h1>
+      </div>
+    );
+  }
+  return (
+    <div className="sidebar">
+      {props.annotations.features.map((feature, index) => <MenuItem key={index} data={feature} />)}
+    </div>
+  );
+};
+
+export default Sidebar;
