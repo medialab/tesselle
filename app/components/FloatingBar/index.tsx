ea9d8801aed96cd73813f74edb24c67467e63099
Robin de Mourat <robin.demourat@gmail.com>
2019-06-19 16:43:25 +0200

    Add tooltips for tools icons with shortcuts close #39

--- app/components/FloatingBar/index.tsx
+++ app/components/FloatingBar/index.tsx
@@ -7,6 +7,7 @@
 import * as React from 'react';
 import Control from 'react-leaflet-control';
 import { Button, Icon } from 'quinoa-design-library';
+import Tooltip from 'react-tooltip';
 import icons from '../../images/icons/';
 import { SupportedShapes } from 'types';
 import './styles.css';
@@ -25,27 +26,33 @@ const FloatingBar: React.SFC<FloatingBarProps> = (props: FloatingBarProps) => {
     icon: icons.select,
     tool: SupportedShapes.selector,
     event: props.onSelectClick,
+    helpMessage: 'select (shortcut: escape)'
   }, {
     icon: icons.anchorRectangle,
     tool: SupportedShapes.rectangle,
     event: props.onRectangleClick,
+    helpMessage: 'rectangle (shortcut: r)'
   }, {
     icon: icons.anchorEllipse,
     tool: SupportedShapes.circle,
     event: props.onCircleClick,
+    helpMessage: 'circle (shortcut: c)'
   }, {
     icon: icons.anchorPolygon,
     tool: SupportedShapes.polygon,
     event: props.onPolygonClick,
+    helpMessage: 'polygon (shortcut: p)'
   }];
 
   return (
     <Control position="bottomright">
       <div className="buttons-container">
-        {buttons.map(({icon, tool, event}) => (
+        {buttons.map(({icon, tool, event, helpMessage}) => (
             <Button
               onClick={event}
               key={tool}
+              data-tip={helpMessage}
+              data-for="tool-tooltip"
               isColor={tool === props.activeButton ? 'primary' : ''}
               style={{marginBottom: '.5rem'}} isRounded>
               <Icon isSize="medium" isAlign="left">
@@ -54,6 +61,7 @@ const FloatingBar: React.SFC<FloatingBarProps> = (props: FloatingBarProps) => {
             </Button>
         ))}
       </div>
+      <Tooltip id="tool-tooltip" place="left" effect="solid" />
     </Control>
   );
 };
