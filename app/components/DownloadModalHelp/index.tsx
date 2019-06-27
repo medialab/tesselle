7f68ba40d37522d4d3e855e360d1ffcd5b44352a
Robin de Mourat <robin.demourat@gmail.com>
2019-06-27 18:05:16 +0200

    improve download help modal content

--- app/components/DownloadModalHelp/index.tsx
+++ app/components/DownloadModalHelp/index.tsx
@@ -55,7 +55,7 @@ const DownloadModalHelp: React.SFC<OwnProps> = (props: OwnProps) => {
             <p>Tesselle data lives in your browser, which means all the work you are doing is private and protected by default.</p>
             <p>In order to get your data out there, the archive downloaded from Tesselle when clicking on the "download" button can be used in two ways:</p>
             <ol>
-              <li>to archive your work, version it, or share it with coworkers, and possibly to re-upload it in the tool later</li>
+              <li>to archive your work, version it, or share it with coworkers, and possibly to re-upload from the homepage of the tool later</li>
               <li>to publish your work on the web</li>
             </ol>
 
@@ -63,7 +63,7 @@ const DownloadModalHelp: React.SFC<OwnProps> = (props: OwnProps) => {
               For the matter of publication, the archived folder which is downloaded from the tool is a plain static website that can be uploaded anywhere on a personal server or web hosting service.
             </p>
             <p>
-              For instance, you can use <a href="https://app.netlify.com/drop" target="blank" rel="noopener">Netlify drop</a> as a free and straightforward way to publish the site of the web. To do so, go to the <a href="https://app.netlify.com/drop" target="blank" rel="noopener">Netlify drop</a> webpage, drag and drop the downloaded file on it, and ... that's it, your document is online !
+              For instance, you can use <a href="https://app.netlify.com/drop" target="blank" rel="noopener">Netlify drop</a> as a free and straightforward way to publish the site of the web. To do so, go to the <a href="https://app.netlify.com/drop" target="blank" rel="noopener">Netlify drop</a> webpage, drag and drop the downloaded archive file on it, and ... that's it, your document is online !
             </p>
             <p>
               As an alternative, you can also use github pages as a publication solution. You can refer to this <a href="https://www.youtube.com/watch?v=8hrJ4oN1u_8" target="blank" rel="noopener">video tutorial</a> for that matter.
