5cc3c60d2b06f2731b54da1f8acdabc308a54e55
Robin de Mourat <robin.demourat@gmail.com>
2019-11-13 15:32:16 +0100

    Updates

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ var __wpo = {
     ],
     "additional": [
       "https://medialab.github.io/tesselle/vendor.738fb51c04446d2bee6e.chunk.js",
-      "https://medialab.github.io/tesselle/main.d7413501544986c20ad8.chunk.js",
+      "https://medialab.github.io/tesselle/main.31bcd5967caf3f6f059f.chunk.js",
       "https://medialab.github.io/tesselle/3.622000ceebbf99688082.chunk.js",
       "https://medialab.github.io/tesselle/4.5bce229ebef2591782e1.chunk.js"
     ],
@@ -17,15 +17,15 @@ var __wpo = {
   "hashesMap": {
     "30b0fcc2b8e05e59779c194a301a133811a5ad5c": "https://medialab.github.io/tesselle/favicon.ico",
     "d0b8b6763fda684f7c696632bb2681f51afd8dd3": "https://medialab.github.io/tesselle/vendor.738fb51c04446d2bee6e.chunk.js",
-    "7c8c434029ee74c77476d5259ff13359e76b25c8": "https://medialab.github.io/tesselle/main.d7413501544986c20ad8.chunk.js",
+    "eb73897aada96d9370d1b5ad0fbb5cd10915bb0b": "https://medialab.github.io/tesselle/main.31bcd5967caf3f6f059f.chunk.js",
     "40d36f2f8633111a7d8113b1def56e94dbd83ffc": "https://medialab.github.io/tesselle/runtime~main.5fe36ceb485b9ba7bd6b.js",
     "a4dd3a66222c45f2dcecab3de214363fd896966e": "https://medialab.github.io/tesselle/3.622000ceebbf99688082.chunk.js",
     "d622d1c83cbd1e7cae0493b080994550a622468b": "https://medialab.github.io/tesselle/4.5bce229ebef2591782e1.chunk.js",
-    "a03fde1c4eab49710932bdef68bf9b4cf34a706b": "https://medialab.github.io/tesselle/"
+    "a65c03dbe6faef8de3543d81a23be7caa4e68989": "https://medialab.github.io/tesselle/"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "2019-11-13 15:25:01",
+  "version": "2019-11-13 15:32:13",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
